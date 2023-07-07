2a3293bde70e99470a8d5199a12a5e6ec050909d
Paulina Kubiak <pola.kubiak@gmail.com>
2023-07-07 16:22:10 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+directions_of_the_world = ["północ", "południe"]
+print(directions_of_the_world)
+directions_of_the_world = directions_of_the_world + ["zachód"]
+directions_of_the_world.append("wschód")
+print(directions_of_the_world)
